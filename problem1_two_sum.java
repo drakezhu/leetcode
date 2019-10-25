216ff99f393bda6633aac0b53b010831770472ee
yunke zhu <drake@yunkes-MacBook-Pro.local>
2019-10-24 23:01:41 -0400

    add some comment to the code

--- problem1_two_sum.java
+++ problem1_two_sum.java
@@ -3,15 +3,23 @@ class Solution {
         
         Map<Integer, Integer> map = new HashMap<>();
     
+        // using hash map to store the array
+
         for (int i = 0; i < nums.length; i++) 
         {
         
             map.put(nums[i], i);
         }
+
+
     
         for (int i = 0; i < nums.length; i++) 
         {   
+            // define the value of i's complement
+
             int complement = target - nums[i];
+
+            // if the complement exists in the map and meanwhile it is not identical to i itself, we return the choosen value
             if (map.containsKey(complement) && map.get(complement) != i) {
                 return new int[] { i, map.get(complement) };
             }
