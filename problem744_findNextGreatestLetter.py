49a921daf972066dc217bf498ca4dc160a093fe6
yunke zhu <drake@resnet136-93.medford.tufts.edu>
2019-10-31 13:10:33 -0400

    problem 965 solved

---
+++ problem744_findNextGreatestLetter.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for cur in letters:
+            if target < cur :
+                return cur
+        return letters[0]
